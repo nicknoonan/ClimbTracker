e92633955f55bfb3688e3cc73544bbc6df07dd55
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2023-02-12 10:37:45 -0500

    Update Program.cs

--- TrackerApi/Program.cs
+++ TrackerApi/Program.cs
@@ -9,18 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-if (builder.Environment.IsProduction())
-{
-    builder.Configuration.AddAzureKeyVault(
-        new Uri($"https://{builder.Configuration["AZURE_KEY_VAULT_NAME"]}.vault.azure.net/"),
-        new DefaultAzureCredential(),
-        new PrefixKeyVaultSecretManager(builder.Configuration["AZURE_KEY_VAULT_SECRET_PREFIX"]));
-}
-else if (builder.Environment.IsDevelopment())
-{
-    builder.Configuration.AddUserSecrets<IConfiguration>();
-    
-}
 
 var app = builder.Build();
 
