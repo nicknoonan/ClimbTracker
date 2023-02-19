7efe0815d72fc502d4bbddb0049a582fb9c7cda6
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2023-02-19 09:47:52 -0500

    Update CacheHealthController.cs

--- TrackerApi/Controllers/CacheHealthController.cs
+++ TrackerApi/Controllers/CacheHealthController.cs
@@ -20,20 +20,23 @@ namespace TrackerApi.Controllers
             _cacheHelper = cacheHelper;
         }
         [HttpGet(Name = "GetCacheHealth")]
-        public async Task<List<string>> Get()
+        public async Task<List<string>> Get(int count)
         {
-            int count = 100;
             List<string> list = new List<string>();
+            bool healthy = await _cacheHelper.CheckConnectionAsync();
+            string health_message = healthy ? "healthy" : "unhealthy";
+            list.Add(health_message);
+            count = (count <= 1000) ? count : 1000;
             for (int i = 0; i < count; i++)
             {
                 string key = String.Format("healthcheck{0}", i.ToString());
                 string value = String.Format("healthcheck{0}",i.ToString());
                 await _cacheHelper.SetAsync(key, Encoding.UTF8.GetBytes(value), new DateTimeOffset(DateTime.UtcNow.AddMinutes(1)));
                 byte[]? fetched_value = await _cacheHelper.GetAsync(key);
-                list.Add(Encoding.UTF8.GetString(fetched_value ?? new byte[]{}));
+                string check = (fetched_value == null) ? String.Format("unhealthy{0}",i.ToString()) : Encoding.UTF8.GetString(fetched_value);
+                list.Add(check);
             }
-            bool healthy = await _cacheHelper.CheckConnectionAsync();
-            string health_message = healthy ? "healthy" : throw new Exception("unhealthy sql cache");
+
             return list;
         }
     }
